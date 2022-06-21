3436182a0ba6c1868df401590e567efc8be593e3
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-21 22:19:13 +0100

    add styling to h1, form and button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#form-machine").submit(function(event) {
-        const fullname1 = ("#fullname-1").val();
-        const email1 = ("#email-1").val();
-        const address1 = ("#address-1").val();
-        const phone1 = ("#phone-1").val();
-        const dob1 = ("#dob-1").val();
+        const fullname1 = $("#fullname-1").val();
+        const email1 = $("#email-1").val();
+        const address1 = $("#address-1").val();
+        const phone1 = $("#phone-1").val();
+        const dob1 = $("#dob-1").val();
 
         $(".fullname-1").text(fullname1);
         $(".email-1").text(email1);
@@ -22,3 +22,4 @@ $(document).ready(function() {
     });
 
 });
+
