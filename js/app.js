dd2c84478781328b953a492b9972be92eab0ecef
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-21 21:16:59 +0100

    add a jquery function to take all user information and print it on postcard

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("#form-machine").submit(function(event) {
+        const fullname1 = ("#fullname-1").val().toUppercase();
+        const email1 = ("#email-1").val().toUppercase();
+        const address1 = ("#address-1").val().toUppercase();
+        const phone1 = ("#phone-1").val().toUppercase;
+        const dob1 = ("#dob-1").val().toUppercase();
+
+        $(".fullname-1").text(fullname1);
+        $(".email-1").text(email1);
+        $(".address-1").text(address1);
+        $(".phone-1").text(phone1);
+        $(".dob-1").text(dob1);
+
+        $("#postcard").show();
+
+        event.preventDefault();
+
+    });
+
+});
