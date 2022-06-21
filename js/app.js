c77317ebc675757892842532375e00ab1f79d311
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-21 21:58:12 +0100

    add comment tosections of the project

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#form-machine").submit(function(event) {
-        const fullname1 = ("#fullname-1").val().toUppercase();
-        const email1 = ("#email-1").val().toUppercase();
-        const address1 = ("#address-1").val().toUppercase();
-        const phone1 = ("#phone-1").val().toUppercase;
-        const dob1 = ("#dob-1").val().toUppercase();
+        const fullname1 = ("#fullname-1").val();
+        const email1 = ("#email-1").val();
+        const address1 = ("#address-1").val();
+        const phone1 = ("#phone-1").val();
+        const dob1 = ("#dob-1").val();
 
         $(".fullname-1").text(fullname1);
         $(".email-1").text(email1);
@@ -15,6 +15,9 @@ $(document).ready(function() {
         $("#postcard").show();
 
         event.preventDefault();
+
+
+
 
     });
 
