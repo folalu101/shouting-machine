b6064c9b80780d2225fbb39ca24a06b909e33df9
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-22 19:11:21 +0100

    add a method to turn all value to upper case on jquery

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#form-machine").submit(function(event) {
-        const fullname1 = $("#fullname-1").val().toUppercase();
-        const email1 = $("#email-1").val().toUppercase();
-        const address1 = $("#address-1").val().toUppercase();
-        const phone1 = $("#phone-1").val().toUppercase();
-        const dob1 = $("#dob-1").val().toUppercase();
+        const fullname1 = $("#fullname-1").val().toUpperCase();
+        const email1 = $("#email-1").val().toUpperCase();
+        const address1 = $("#address-1").val().toUpperCase();
+        const phone1 = $("#phone-1").val().toUpperCase();
+        const dob1 = $("#dob-1").val().toUpperCase();
 
         $(".fullname-1").text(fullname1);
         $(".email-1").text(email1);
